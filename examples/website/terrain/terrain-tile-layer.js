409b84d1602bc4c60f7391cdca3ea6e779cc0c05
Kyle Barron <barronk@mit.edu>
2020-03-09 19:23:46 -0600

    Working geojson layer

--- examples/website/terrain/terrain-tile-layer.js
+++ examples/website/terrain/terrain-tile-layer.js
@@ -4,6 +4,8 @@ import {load} from '@loaders.gl/core';
 import {TerrainLoader} from '@loaders.gl/terrain';
 import {TileLayer} from '@deck.gl/geo-layers';
 import {TERRAIN_IMAGE, NAIP_IMAGE, SURFACE_IMAGE, ELEVATION_DECODER} from './util';
+import {GeoJsonLayer} from '@deck.gl/layers';
+import {MVTLoader} from '@loaders.gl/mvt';
 
 const MESH_MAX_ERROR = 10;
 const DUMMY_DATA = [1];
@@ -26,8 +28,13 @@ function getTextureUrl({x, y, z}) {
     .replace('{z}', z);
 }
 
+function getOpenMapTilesUrl({x, y, z}) {
+  return `https://mbtiles.nst.guide/services/openmaptiles/own/tiles/${z}/${x}/${y}.pbf`;
+}
+
 export function TerrainTileLayer() {
   return new TileLayer({
+    id: 'terrain-tiles',
     minZoom: 0,
     maxZoom: 17,
     getTileData,
@@ -38,6 +45,7 @@ export function TerrainTileLayer() {
 function getTileData({x, y, z, bbox}) {
   const terrainUrl = getTerrainUrl({x, y, z});
   const textureUrl = getTextureUrl({x, y, z});
+  const mvtUrl = getOpenMapTilesUrl({x, y, z});
 
   const viewport = new WebMercatorViewport({
     longitude: (bbox.west + bbox.east) / 2,
@@ -48,6 +56,17 @@ function getTileData({x, y, z, bbox}) {
   const topRight = viewport.projectFlat([bbox.east, bbox.north]);
   const bounds = [bottomLeft[0], bottomLeft[1], topRight[0], topRight[1]];
 
+  const mvtLoaderOptions = {
+    mvt: {
+      coordinates: 'wgs84',
+      tileIndex: {
+        x,
+        y,
+        z
+      }
+    }
+  };
+  const mvttile = load(mvtUrl, MVTLoader, mvtLoaderOptions);
   const terrain = loadTerrain({
     terrainImage: terrainUrl,
     bounds,
@@ -59,24 +78,28 @@ function getTileData({x, y, z, bbox}) {
       load(textureUrl).catch(_ => null)
     : Promise.resolve(null);
 
-  return Promise.all([terrain, texture]);
+  return Promise.all([terrain, texture, mvttile]);
 }
 
 function renderSubLayers(props) {
-  const {data} = props;
+  const {data, tile} = props;
   let mesh = null;
   let texture = null;
+  let geojsonFeatures = null;
 
   if (Array.isArray(data)) {
     mesh = data[0];
     texture = data[1];
+    geojsonFeatures = data[2];
   } else if (data) {
     mesh = data.then(result => result && result[0]);
     texture = data.then(result => result && result[1]);
+    geojsonFeatures = data.then(result => result && result[2]);
   }
 
   return [
     new SimpleMeshLayer(props, {
+      id: `terrain-simple-mesh-layer-${tile.x}-${tile.y}-${tile.z}`,
       data: DUMMY_DATA,
       mesh,
       texture,
@@ -85,6 +108,11 @@ function renderSubLayers(props) {
       getPosition: d => [0, 0, 0],
       // Color to use if surfaceImage is unavailable
       getColor: [255, 255, 255]
+    }),
+    new GeoJsonLayer(props, {
+      // NOTE: currently you need to set each sublayer id so they don't conflict
+      id: `geojson-layer-${tile.x}-${tile.y}-${tile.z}`,
+      data: geojsonFeatures.then(r => r)
     })
   ];
 }
